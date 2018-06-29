73751083a94a084ce99495c7d299a74877ba10c1
Romesh McCullough <romesh.mccullough@gmail.com>
2018-06-29 18:03:32 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,106 @@
-# go-ecs-deploy
-Helper tool to deploy ECS services from CI systems
+
+go-ecs-deploy
+=============
+
+## Overview
+
+A small library to take make deploying to AWS ECS from remote systems (like CI servers) a little easier.  It supports deploying both long running services attached to a load balancer as well as one-shot tasks that should run then exit.
+
+### Services
+A service is specifically an [ECS Service](https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs_services.html).  It is expected to start and remain running indefinitely.
+
+### Oneshots
+A oneshot service is expected to run and exit.  It uses [ECS RunTask](https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs_run_task.html) under the hood.  Oneshots are useful for running stand-alone database migration containers, or other ad-hoc one off tasks.
+
+## CLI Tool
+
+The included CLI tool loads a config file and a template.  The template file is first executed with the variables in the execution environment, then again with the specified config files.  Values set in the config file will override values set in the environment.
+
+Default templates are located in the `templates/` directory, with some example config files under `example/`
+
+## Usage
+AWS credentials must be available in the usual ways (metadata endpoint, credentials file, environment vars).
+
+**Note:** the CLI tool does not yet support profiles, as the intended place to run is a CI job that most likely has env vars or an instance profile set.
+
+**Deploy a service:**
+
+Use only values in the config file:
+```sh
+go run src/main.go -config example/service-a/service-full.json -template templates/service.json.tpl -type service
+```
+
+Set some vars in the environment and the rest in a config file.  This is useful for substituting runtime CI variables that change frequently.
+```sh
+ImageTag=sometag Image=registry/image go run src/main.go -config example/service-a/service-full.json -template templates/service.json.tpl -type service
+```
+
+The above commands will:
+* replace any template vars with environment variables first, then the config file.
+* register a new task definition
+* create a new service or update an existing matching service
+    * If the service already exists the service template `InitialCount` will be changed to match the `DesiredCount` in ECS, so as to not undo any manual or auto scaling actions.
+* block waiting for the service to be stable 
+
+
+## Config Examples
+A full service config.
+```json
+{
+    "ServiceName": "service-a",
+    "Cluster": "some-cluster",
+    "SchedulerIAMRoleArn": "arn",
+    "TaskIAMRoleArn": "arn",
+    "TargetGroupArn": "arn",
+    "InitialCount": 2,
+    "Image": "registry/container",
+    "ImageTag": "xyz",
+    "MemoryReservation": 128,
+    "CPUReservation": 128,
+    "AWSLogsGroupName": "name",
+    "AWSLogsRegion": "region",
+    "Environment": [
+        {
+            "name": "ENVIRONMENT",
+            "value": "dev"
+        }
+    ]
+}
+```
+
+
+A partial service config that will require the missing vars to be set in the environment.  Note that `Image` and `ImageTag` are omitted.
+```json
+{
+    "ServiceName": "service-a",
+    "Cluster": "some-cluster",
+    "SchedulerIAMRoleArn": "arn",
+    "TaskIAMRoleArn": "arn",
+    "TargetGroupArn": "arn",
+    "InitialCount": 2,
+    "MemoryReservation": 128,
+    "CPUReservation": 128,
+    "AWSLogsGroupName": "name",
+    "AWSLogsRegion": "region",
+    "Environment": [
+        {
+            "name": "ENVIRONMENT",
+            "value": "dev"
+        }
+    ]
+}
+```
+
+## Included templates
+
+The templates in `templates/` are very opinionated and made for common workflows that we have.  Specifically:
+
+* `templates/migration.json.tpl` for running a container with [golang-/migrate](https://github.com/golang-migrate/migrate).  Note the `command` override.
+* `templates/service.json.tpl` for running services with some default settings.
+
+## Notes
+
+Currently this tool assumes only 1 container definition per task:
+* the health check waiters only inspect the first container in the service.
+* the environment `[]map[string]string` is only overridden for the first container def
+
---
+++ src/main.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/away-team/go-ecs-deploy/src/ecsdeploy"
+	"github.com/aws/aws-sdk-go/aws"
+
+	"github.com/alecthomas/template"
+	"github.com/aws/aws-sdk-go/service/ecs"
+	"github.com/mitchellh/mapstructure"
+)
+
+// ./deploy -config <file> -template <file> -type ( oneshot | service ) [ -timeout int ]
+
+const (
+	defaultTimeout            = 360 * time.Second
+	configFilename            = "config.json"
+	environmentConfigFilename = "environment.json"
+	ServiceTypeService        = "service"
+	ServiceTypeOneshot        = "oneshot"
+)
+
+type EnvironmentConfig struct {
+	TaskFamily          string
+	ServiceName         string
+	Cluster             string
+	SchedulerIAMRoleArn string
+	TaskIAMRoleArn      string
+	TargetGroupArn      string
+	InitialCount        int64
+	Image               string
+	ImageTag            string
+	MemoryReservation   int64
+	CPUReservation      int64
+	AWSLogsGroupName    string
+	AWSLogsRegion       string
+	Environment         []map[string]string
+}
+
+type Config struct {
+	Service ecs.CreateServiceInput
+	Task    ecs.RegisterTaskDefinitionInput
+}
+
+func main() {
+	config := flag.String("config", "", "The path to the config file to use")
+	tpl := flag.String("template", "", "The path to the template to use")
+	serviceType := flag.String("type", "", "The type of service to deploy {service | oneshot}")
+	debug := flag.Bool("debug", false, "Print the the rendered template and exit without deploying")
+	flag.Parse()
+
+	if *config == "" {
+		log.Println("-config cannot be blank")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if *tpl == "" {
+		log.Println("-template cannot be blank")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if *serviceType == "" {
+		log.Println("type cannot be blank")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if *serviceType != ServiceTypeService && *serviceType != ServiceTypeOneshot {
+		log.Printf("Invalid service type: (%s)", *serviceType)
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	var envConfig EnvironmentConfig
+
+	// load environment vars into a map and overlay onto envConfig
+	environment := getEnv()
+	err := mapstructure.Decode(environment, &envConfig)
+	if err != nil {
+		log.Printf("Error decoding environment: %v", err)
+		os.Exit(1)
+	}
+
+	// clean up the passed in config dir path
+	configPath := filepath.Clean(*config)
+	confgBytes, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		log.Printf("Failed reading config file: %v", err)
+		os.Exit(1)
+	}
+
+	// Overlay config file over envConfig populated by env vars
+	err = json.Unmarshal(confgBytes, &envConfig)
+	if err != nil {
+		log.Printf("Failed parsing config file: %v", err)
+		os.Exit(1)
+	}
+
+	// clean up the passed in template dir path
+	tplPath := filepath.Clean(*tpl)
+	f, err := ioutil.ReadFile(tplPath)
+	if err != nil {
+		log.Printf("Error loading template: %v", err)
+		os.Exit(1)
+	}
+
+	c, err := runTemplate(f, envConfig)
+	if err != nil {
+		log.Printf("Failed parsing template: %v", err)
+		os.Exit(1)
+	}
+
+	var conf Config
+	err = json.Unmarshal(c, &conf)
+	if err != nil {
+		log.Printf("Failed parsing template result to config: %v", err)
+		os.Exit(1)
+	}
+
+	// Convert the config environment map to the proper type for an ECS ContainerDefinition.
+	// TODO: figure out how to actually render a go map into a json map correctly, and handle multi container taks.
+	env := make([]*ecs.KeyValuePair, 0)
+	for _, m := range envConfig.Environment {
+		env = append(env, &ecs.KeyValuePair{Name: aws.String(m["name"]), Value: aws.String(m["value"])})
+	}
+	conf.Task.ContainerDefinitions[0].Environment = env
+
+	if *debug {
+		fmt.Println(string(c))
+		os.Exit(0)
+	}
+
+	// Deploy
+
+	// Create the deployer
+	deployer := ecsdeploy.NewECSDeployer(envConfig.Cluster)
+	var deployErr error
+
+	if *serviceType == ServiceTypeService {
+		log.Printf("Deploying service...")
+		deployErr = deployer.DeployService(&conf.Task, &conf.Service)
+	} else {
+		log.Printf("Deploying oneshot...")
+		deployErr = deployer.DeployOneshot(&conf.Task)
+	}
+
+	if deployErr != nil {
+		log.Fatalf("Deployment failed: %v", deployErr)
+	}
+
+	log.Printf("Deploy success")
+	os.Exit(0)
+}
+
+func getEnv() map[string]string {
+	env := make(map[string]string)
+	for _, v := range os.Environ() {
+		pair := strings.SplitN(v, "=", 2)
+		env[pair[0]] = pair[1]
+	}
+	return env
+}
+
+func runTemplate(tpl []byte, data interface{}) ([]byte, error) {
+
+	t, err := template.New("config").Parse(string(tpl))
+	if err != nil {
+		return nil, fmt.Errorf("Failed parsing template: %v", err)
+	}
+
+	var buf bytes.Buffer
+	err = t.Execute(&buf, data)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
